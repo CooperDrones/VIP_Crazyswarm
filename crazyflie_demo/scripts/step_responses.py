4375eb0f4953c18a01c9923c9b3380b4701692a0
Harry Wei <harry98530@gmail.com>
2019-10-17 17:30:06 -0400

    step responses is throwing nan's for historical error. Started working on go to script

--- crazyflie_demo/scripts/step_responses.py
+++ crazyflie_demo/scripts/step_responses.py
@@ -7,6 +7,7 @@ from std_msgs.msg import Duration
 from vicon_bridge.srv import viconGrabPose
 import numpy as np
 from scipy.spatial.transform import Rotation
+import math
 
 class Tester:
     def __init__(self):
@@ -34,11 +35,21 @@ class Tester:
 
     def getPose(self, vicon_object):
         self.pose = self.pose_getter(vicon_object, vicon_object, 1)
-        return self.pose.pose.pose.position
+        self.position = self.pose.pose.pose.position
+        self.position_list = [self.position.x, self.position.y, self.position.z]
+        if math.isnan(self.position_list[0]):
+            self.posiiton_list = [0, 0, 0]
+        return self.position_list
     
     def getPoseQuaternion(self, vicon_object):
         self.pose = self.pose_getter(vicon_object, vicon_object, 1)
-        return self.pose.pose.pose.orientation
+        self.quat = self.pose.pose.pose.orientation
+        self.quat_list = [self.quat.x, self.quat.y, self.quat.z, self.quat.w]
+        # if math.isnan(self.position_list[0]):
+        self.quat_list[0] = float
+        if math.isnan(float(self.quat_list[0]))
+            self.quat_list = [0, 0, 0, 1]
+        return self.quat_list
 
     def commandAction(self, roll, pitch, thrust, yaw_rate, action_time):
         self.twist_msg.linear = Vector3(roll, pitch, thrust)
@@ -79,51 +90,61 @@ class Tester:
 
             print("The commanded thrust is: {}".format(self.twist_msg.linear.z))
             print("The error is: {}".format(self.z_error_scaled))
-            self.pub_twist.publish(self.twist_msg)
+            # self.pub_twist.publish(self.twist_msg)
     
     def hoverWithBangBang(self, z_ref, circle_radius):
-        self.counter = 0
-        self.z_feed_forward = 39000
-        self.z_kp = 10000
-        self.feed_forward = 39000
-        self.xy_kp = 10
-        self.yaw_kp = -30
-        self.x_origin = 0
-        self.y_origin = 0
+        self.counter = 0.
+        self.z_feed_forward = 39000.
+        self.z_kp = 10000.
+        self.z_ki = 1000.
+        self.z_error_historical = 0.
+        self.z_feed_forward = 39000.
+        self.xy_kp = 10.
+        self.yaw_kp = -30.
+        self.x_origin = 0.
+        self.y_origin = 0.
+        self.z_error_cap = 9.9
         while not rospy.is_shutdown():
-            self.pose_cf = self.getPose('crazyflie2')
-            self.x_actual = self.pose_cf.x
-            self.y_actual = self.pose_cf.y
-            self.z_actual = self.pose_cf.z
+            self.position_actual = self.getPose('crazyflie2')
+            # print("The position is: {}".format(self.position_actual))
 
-            self.z_error = z_ref - self.z_actual
-            # self.z_error_new += self.z_error
-
-            self.z_error_scaled = self.z_error * self.z_kp
-            self.twist_msg.linear.z = self.feed_forward + self.z_error_scaled
-            self.twist_msg.linear.x = -1 # Trim based on observation
+            # Trim based on observation
+            self.twist_msg.linear.x = -1
             self.twist_msg.linear.y = 1
-            
-            # Initialize lateral bang bang controller after a time delay
-            if self.counter >= 15:
-                self.offset = np.sqrt((self.x_actual - self.x_origin)**2 + (self.y_actual - self.y_origin)**2)
-                self.theta = np.arctan2((self.y_actual - self.y_origin), (self.x_actual - self.x_origin))
-                print("")
-                if self.offset > circle_radius:
-                    self.twist_msg.linear.x = -1 * self.xy_kp * self.offset * np.cos(self.theta)
-                    self.twist_msg.linear.y = self.xy_kp * self.offset * np.sin(self.theta)
-                    # print("Lateral controller triggered, offset: {}, theta: {}".format(self.offset, self.theta))
-            
-            self.quat_actual = self.getPoseQuaternion('crazyflie2')
-            self.quat_actual = [self.quat_actual.x, self.quat_actual.y, self.quat_actual.z, self.quat_actual.w]
-            R = Rotation.from_quat(self.quat_actual)
-            self.global_x = R.apply([1, 0, 0])
-            self.yaw_angle = (-1) * np.arctan2(np.cross([1, 0, 0], self.global_x)[2], np.dot(self.global_x, [1, 0, 0]))
-            print(self.yaw_angle)
-            self.twist_msg.angular.z = self.yaw_kp * self.yaw_angle
 
-            self.pub_twist.publish(self.twist_msg)
-            self.counter += 1
+            # Altitude controller
+            self.z_actual = self.position_actual[2]
+            self.z_error = z_ref - self.z_actual
+            if self.z_error_historical <= self.z_error_cap:
+                self.z_error_historical += (self.z_error * 1/self.hz)
+            self.z_error_scaled = (self.z_error * self.z_kp) + (self.z_error_historical * self.z_ki)
+            self.twist_msg.linear.z = self.z_feed_forward + self.z_error_scaled
+            
+            # Lateral bang bang controller - provides restoring pitch and yaw if drone excapes circle on xy plane
+            # if self.counter >= 15:
+            self.x_actual = self.position_actual[0]
+            self.y_actual = self.position_actual[1]
+            self.offset = np.sqrt((self.x_actual - self.x_origin)**2 + (self.y_actual - self.y_origin)**2)
+            self.theta = np.arctan2((self.y_actual - self.y_origin), (self.x_actual - self.x_origin))
+            if self.offset > circle_radius:
+                self.twist_msg.linear.x = -1 * self.xy_kp * self.offset * np.cos(self.theta)
+                self.twist_msg.linear.y = self.xy_kp * self.offset * np.sin(self.theta)
+                # print("Lateral controller triggered, offset: {}, theta: {}".format(self.offset, self.theta))
+            
+            # # Yaw-rate controller
+            self.quat_actual = self.getPoseQuaternion('crazyflie2')
+            print("The orientation is: {} with type {}".format(self.quat_actual[0], type(self.quat_actual[0])))
+            # R = Rotation.from_quat(self.quat_actual)
+            # self.global_x = R.apply([1, 0, 0])
+            # self.yaw_angle = (-1) * np.arctan2(np.cross([1, 0, 0], self.global_x)[2], np.dot(self.global_x, [1, 0, 0]))
+            # print(self.yaw_angle)
+            # self.twist_msg.angular.z = self.yaw_kp * self.yaw_angle
+
+            # print("The commanded thrust is: {}".format(self.twist_msg.linear.z))
+            # print("The error is {}. Historical error is: {}".format(self.z_error, self.z_error_historical))
+
+            # self.pub_twist.publish(self.twist_msg)
+            # self.counter += 1
 
 
 if __name__ == "__main__":
