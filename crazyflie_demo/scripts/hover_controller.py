15e44b783750c4e8e97ee6528739beac180af818
Justin Rooney <justin@dexai.com>
2019-11-11 13:58:48 -0500

    pre-camera move scripts

--- crazyflie_demo/scripts/hover_controller.py
+++ crazyflie_demo/scripts/hover_controller.py
@@ -12,7 +12,7 @@ import math
 class Tester:
     def __init__(self):
         self.msg = Twist()
-        self.hz = 30 # if not set to 100, will not broadcast
+        self.hz = 30.0 # if not set to 100, will not broadcast
         self.rate = rospy.Rate(self.hz)
 
         self.pub = rospy.Publisher('crazyflie/cmd_vel', Twist, queue_size=0)
@@ -48,34 +48,50 @@ class Tester:
             self.pub.publish(self.msg)
             self.rate.sleep()
     
-    def hoverWithBangBang(self, z_ref, circle_radius):
-        # overcome initial thrust command block
+    def hoverWithBangBang(self, z_ref, circle_radius, x_ref, y_ref):
+        # REQUIRED TO OVERCOME INITIAL PUBLISHER BLOCK IMPLEMENTED BY USC
         self.msg.linear = Vector3(0, 0, 0)
         self.msg.angular = Vector3(0, 0, 0)
         for i in range(100):
             self.pub.publish(self.msg)
             self.rate.sleep()
         
-        # start custom controller
+        # Altitude (z) controller gains
         self.z_feed_forward = 45000.
         self.z_kp = 11000.
         self.z_ki = 3500.
-        self.z_kd = 1000
+        self.z_kd = 9000
         self.z_error_historical = 0.
         self.z_feed_forward = 39000.
-        self.xy_kp = 10.
-        self.yaw_kp = -30.
-        self.x_origin = 0.
-        self.y_origin = 0.
         self.z_error_cap = 1.5 # prevent wind up
         self.z_error_before = 0.
+        
+        # BANG BANG gains
+        self.xy_kp = 10.
+        
+        # XY controller gains
+        self.x_kp = 10.
+        self.x_ki = 2.
+        self.y_kp = 10.
+        self.y_ki = 2.
+
+        self.x_error_historical = 0.
+        self.y_error_historical = 0.
+
+        # Yaw rate controller gains
+        self.yaw_kp = -30.
+
+        # self.x_origin = 0.
+        # self.y_origin = 0.
+
+        time_step = (1/self.hz)
         while not rospy.is_shutdown():
             self.position_actual = self.getPose('crazyflie3')
             # print("The position is: {}".format(self.position_actual))
 
             # Trim based on observation
-            self.msg.linear.x = -1
-            self.msg.linear.y = 1
+            self.msg.linear.x = 0
+            self.msg.linear.y = 0
 
             # Altitude controller
             self.z_actual = self.position_actual[2]
@@ -85,18 +101,18 @@ class Tester:
             
             # Integral controller with bound
             if self.z_error_historical <= self.z_error_cap:
-                self.z_error_historical += (self.z_error * 1/self.hz)
+                self.z_error_historical += (self.z_error * time_step)
             
             # Derivative controller
             # print(0.03333)
             # print(self.z_error - self.z_error_before)
 
-            self.z_error_der = (self.z_error - self.z_error_before) / (0.03333)
-            self.z_error_before = self.z_error
+            self.z_error_der = (self.z_error - self.z_error_before) / (0.03333) # HERE
+            self.z_error_before = self.z_error # HERE
 
             # Add errors together and multiply by gains
             self.z_error_scaled = (self.z_error * self.z_kp) + (self.z_error_historical * self.z_ki) \
-                + (self.z_error_der * self.z_kd)
+                + (self.z_error_der * self.z_kd) # HERE
 
             # publish to thrust command
             self.msg.linear.z = self.z_feed_forward + self.z_error_scaled
@@ -106,25 +122,38 @@ class Tester:
             self.x_actual = self.position_actual[0]
             self.y_actual = self.position_actual[1]
 
-            self.offset = np.sqrt((self.x_actual - self.x_origin)**2 + (self.y_actual - self.y_origin)**2)
-            self.theta = np.arctan2((self.y_actual - self.y_origin), (self.x_actual - self.x_origin))
+            # BANG BANG Controller
+            # self.offset = np.sqrt((self.x_actual - self.x_origin)**2 + (self.y_actual - self.y_origin)**2)
+            # self.theta = np.arctan2((self.y_actual - self.y_origin), (self.x_actual - self.x_origin))
 
-            self.msg.linear.x = -1 * self.xy_kp * self.offset * np.cos(self.theta)
-            self.msg.linear.y = self.xy_kp * self.offset * np.sin(self.theta)
-            # print("Lateral controller triggered, offset: {}, theta: {}".format(self.offset, self.theta))
-            
+            # self.msg.linear.x = -1 * self.xy_kp * self.offset * np.cos(self.theta)
+            # self.msg.linear.y = self.xy_kp * self.offset * np.sin(self.theta)
+
+            self.x_error = x_ref - self.x_actual
+            self.y_error = y_ref - self.y_actual
+
+            self.x_error_historical += (self.z_error * time_step)
+            self.y_error_historical += (self.z_error * time_step)
+
+            self.x_error_scaled = (self.x_error * self.x_kp) + (self.x_error_historical * self.x_ki)
+            self.y_error_scaled = (self.y_error * self.y_kp) + (self.y_error_historical * self.y_ki)
+
+            self.msg.linear.x = self.x_error_scaled
+            self.msg.linear.y = -self.y_error_scaled
+
             # Yaw-rate controller
             self.quat_actual = self.getPoseQuaternion('crazyflie3')
-            # print("The orientation is: {} with type {}".format(self.quat_actual[0], type(self.quat_actual[0])))
             R = Rotation.from_quat(self.quat_actual)
-            self.global_x = R.apply([1, 0, 0])
+            self.global_x = R.apply([1, 0, 0]) # project to world x-axis
             self.yaw_angle = (-1) * np.arctan2(np.cross([1, 0, 0], self.global_x)[2], np.dot(self.global_x, [1, 0, 0]))
-            # print(self.yaw_angle)
             self.msg.angular.z = self.yaw_kp * self.yaw_angle
 
-            print("The commanded thrust is: {}".format(self.msg.linear.z))
-            print("The error is {}. Historical error is {}. Derivatice error is {}. Total scaled error is: {}"\
-                .format(self.z_error, self.z_error_historical, self.z_error_der, self.z_error_scaled))
+            # print("The commanded thrust is: {}".format(self.msg.linear.z))
+            # print("The z error is {}. Historical error is {}. Derivatice error is {}. Total scaled error is: {}"\
+            #     .format(self.z_error, self.z_error_historical, self.z_error_der, self.z_error_scaled)) # HERE
+            # print("The X error is {}. Y error is {}."\
+                # .format(self.x_error_scaled ,self.y_error_scaled))
+            # print("The orientation is: {} with type {}".format(self.quat_actual[0], type(self.quat_actual[0])))
 
             self.pub.publish(self.msg)
             self.rate.sleep()
@@ -153,7 +182,7 @@ if __name__ == "__main__":
         # test1.hoverWithFeedback(z_ref)
 
         z_ref = 0.5; circle_radius = 0.1
-        test1.hoverWithBangBang(z_ref, circle_radius)
+        test1.hoverWithBangBang(z_ref, circle_radius, 0.0, 0.0)
 
         # test1.listenerTest()
 
