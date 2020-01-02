4162284fa7d643650d0caacf7dabe41678d1358f
Justin Rooney <rooneyjc@gmail.com>
2020-01-02 08:08:20 -0500

    feat: euler and rk4 integrators working
    Implemeted state space representation which is used to calculate the derivative of the state. Implemented euler and rk4 integrators to update state from state derivative.

--- crazyflie_model/crazyflie_dynamics.py
+++ crazyflie_model/crazyflie_dynamics.py
@@ -8,15 +8,15 @@ class Quadrotor:
             [P.x0],
             [P.y0],
             [P.z0],
+            [P.psi0],
+            [P.theta0],
+            [P.phi0],
             [P.u0],
             [P.v0],
             [P.w0],
-            [P.phi0],
-            [P.theta0],
-            [P.psi0],
-            [P.p0],
-            [P.q0],
             [P.r0],
+            [P.q0],
+            [P.q0],
         ])
         # Time step
         self.Ts = P.Ts
@@ -24,34 +24,65 @@ class Quadrotor:
         self.m = P.m
         # Gravitation acceleration
         self.g = P.g
-        # Actuator limits
-        self.input_limits = np.array([
-            [P.phi_max],
-            [P.theta_max],
-            [P.r_max],
-            [P.thrust_max],
-        ])
+        # Hover RPM
+        self.omega_e = P.omega_e
+
+        # Hover state space representation 
+        # A and B matrix
+        self.A = P.A
+        self.B = P.B
+        
+        # Outer Loop Actuator limits
+        self.ol_input_limits = P.ol_input_limits
+        # Prop RPM Actuator Limits
+        self.input_limits = P.input_limits
 
         # TODO inner loop rate controller 
         # IN: pc, qc, rc, p, q, r 
         # OUT: del_phi, del_theta, del_gamma
-    
 
     def update(self, u):
-        # External method that takes input u at time t
-        # and returns the output y at time t
-
-        # TODO u refers to omega1-4 here, NOT control inputs
-        # Find relationship from control inputs 
+        """
+        External method that takes outer loop control commands
+        and updates the state
+        """
         u = self.saturate(u, self.input_limits)
-        # self.rk4_step(u) # propagate the state by one time sample
-        self.euler_step(u)
+        self.rk4_step(u) # propagate the state by one time sample
+        # self.euler_step(u)
         y = self.h() # return the corresponding output
         return y
     
+    def state_dot(self, state, u):
+        # Returns the deivative of the state vector
+        # Uses state-space equations provided on pg. 15
+        xdot = self.A @ self.state + self.omega_e * (self.B @ u)
+        return xdot
+    
+    def h(self):
+        # Returns y = h(x) - Finds position and orientation
+        # from state and combines into the output vector
+        y = np.array([
+            [self.state.item(0)], # x
+            [self.state.item(1)], # y
+            [self.state.item(2)], # z
+            [self.state.item(3)], # psi
+            [self.state.item(4)], # theta
+            [self.state.item(5)], # pi
+        ])
+        return y
+
     def euler_step(self, u):
-        
-        self.state += self.Ts * ()
+        # Integrate ODE using Euler's method
+        xdot = self.state_dot(self.state, u)
+        self.state += self.Ts * xdot
+
+    def rk4_step(self, u):
+        # Integrate ODE using Runge-Kutta RK4 algorithm
+        F1 = self.state_dot(self.state, u)
+        F2 = self.state_dot(self.state + self.Ts / 2 * F1, u)
+        F3 = self.state_dot(self.state + self.Ts / 2 * F2, u)
+        F4 = self.state_dot(self.state + self.Ts * F3, u)
+        self.state += self.Ts / 6 * (F1 + 2 * F2 + 2 * F3 + F4)
 
     def saturate(self, u, limit):
         for idx in range(u.shape[0]):
@@ -62,14 +93,39 @@ class Quadrotor:
 # Run some tests to see functionality
 if __name__ == "__main__":
     cf1 = Quadrotor()
-    # print(cf1.state)
-    u = np.array([
-        [40.0],
-        [-50.0],
-        [500.0],
-        [70000.0],
-    ])
-    u = cf1.saturate(u, cf1.input_limits)
-    cf1.euler_step(u)
 
-    print(cf1.state)
+    # # Test saturation function with external control commands
+    # # roll, pitch, yaw rate, thrust
+    # olu = np.array([
+    #     [40.0],
+    #     [-50.0],
+    #     [500.0],
+    #     [70000.0],
+    # ])
+    # olu = cf1.saturate(olu, cf1.input_limits)
+    # print(olu)
+
+    # Symmetric input
+    u = np.array([
+        [10000],
+        [10000],
+        [10000],
+        [10000],
+    ])
+
+    # # Test euler integrator with symmetric input [RPM] 
+    # print("Euler's method\n", cf1.state)
+    # for _ in range(3):
+    #     cf1.euler_step(u)
+    #     print(cf1.state)
+
+    # # Test rk4 integrator with symmetric input [RPM]
+    cf2 = Quadrotor()
+    print("RK4\n", cf2.state)
+    for _ in range(3):
+        cf2.rk4_step(u)
+        print(cf2.state)
+
+    # for _ in range(3):
+    #     y = cf1.update(u)
+    #     print(y)
--- crazyflie_model/crazyflie_param.py
+++ crazyflie_model/crazyflie_param.py
@@ -3,28 +3,29 @@ import numpy as np
 g = 9.81 # [m/s^2]
 
 # Physical Parameters of the Crazyflie 2.0
-m = 0.35        # Crazyflie 2.0 mass w/ 4 Vicon markers [g]
-Ct = 3.1582e-10 # Thrust coeff [N/rpm^2]
-Cd = 7.9379e-10 # Drag coeff [Nm/rpm^2]
-Ixx = 1.395e-5  # [Kg x m^2]
-Iyy = 1.436e-5  # [Kg x m^2]
-Izz = 2.173e-5  # [Kg x m^2]
-d = 39e-3       # Crazyflie 2.0 arm length [m]
+m = 0.35          # Crazyflie 2.0 mass w/ 4 Vicon markers [g]
+Ct = 3.1582e-10   # Thrust coeff [N/rpm^2]
+Cd = 7.9379e-10   # Drag coeff [Nm/rpm^2]
+Ixx = 1.395e-5    # Moment of inertia [Kg x m^2]
+Iyy = 1.436e-5    # Moment of inertia [Kg x m^2]
+Izz = 2.173e-5    # Moment of inertia [Kg x m^2]
+d = 39e-3         # Crazyflie 2.0 arm length [m]
+omega_e = 16073.0 # Hover RPM [RPM]
 
 # Initial Conditions, state vector formed following:
 # https://arxiv.org/pdf/1608.05786.pdf
 x0 = 0.0     # X position [m]
 y0 = 0.0     # Y position [m]
 z0 = 0.0     # Z position [m]
+psi0 = 0.0   # Yaw angle [rad]
+theta0 = 0.0 # Pitch angle [rad]
+phi0 = 0.0   # Roll angle [rad]
 u0 = 0.0     # Body frame X linear velocity [m/s]
 v0 = 0.0     # Body frame Y linear velocity [m/s]
 w0 = 0.0     # Body frame Z linear velocity [m/s]
-phi0 = 0.0   # Roll angle [rad]
-theta0 = 0.0 # Pitch angle [rad]
-psi0 = 0.0   # Yaw angle [rad]
-p0 = 0.0     # Body frame roll angular velocity [rad/s]
-q0 = 0.0     # Body frame pitch angular velocity [rad/s]
 r0 = 0.0     # Body frame yaw angular velocity [rad/s]
+q0 = 0.0     # Body frame pitch angular velocity [rad/s]
+p0 = 0.0     # Body frame roll angular velocity [rad/s]
 
 # Simulation Parameters
 t_start = 0.0 # [s]
@@ -32,43 +33,53 @@ t_end = 50.0  # [s]
 Ts = 0.01     # [s]
 t_plot = 0.1  # [s]
 
-# Saturation Limits
+# Prop RPM saturation limits
+omega_max = 60000
+input_limits = np.full((4, 1), omega_max)
+
+# Outer loop saturation Limits
 phi_max = 30.0       # [rad]
 theta_max = 30.0     # [rad]
 r_max = 200.0        # [rad/s]
 thrust_max = 60000.0 # mapped to PWM output [N/A]
+ol_input_limits = np.array([
+    [phi_max],
+    [theta_max],
+    [r_max],
+    [thrust_max],
+])
 
-# # Construction of the necessary matrices
-# N = 12
-# M = 4
-# A = np.zeros((N, N))
+# Construction of the necessary state-space matrices
+N = 12
+M = 4
+A = np.zeros((N, N))
 
-# A = np.zeros((N, N))
-# A[0, 6] = 1.
-# A[1, 7] = 1.
-# A[2, 8] = 1.
-# A[3, 9] = 1.
-# A[4, 10] = 1.
-# A[5, 11] = 1.
-# A[6, 4] = g
-# A[7, 5] = -g
+A = np.zeros((N, N))
+A[0, 6] = 1.
+A[1, 7] = 1.
+A[2, 8] = 1.
+A[3, 9] = 1.
+A[4, 10] = 1.
+A[5, 11] = 1.
+A[6, 4] = g
+A[7, 5] = -g
 
-# B = np.zeros((N, M))
-# B[8, 0] = 2. * (Ct/m)
-# B[8, 1] = 2. * (Ct/m)
-# B[8, 2] = 2. * (Ct/m)
-# B[8, 3] = 2. * (Ct/m)
-# B[9, 0] = -2. * (Cd/Izz)
-# B[9, 1] = 2. * (Cd/Izz)
-# B[9, 2] = -2. * (Cd/Izz)
-# B[9, 3] = 2. * (Cd/Izz)
-# B[10, 0] = -np.sqrt(2) * d * (Ct/Iyy)
-# B[10, 1] = np.sqrt(2) * d * (Ct/Iyy)
-# B[10, 2] = np.sqrt(2) * d * (Ct/Iyy)
-# B[10, 3] = -np.sqrt(2) * d * (Ct/Iyy)
-# B[11, 0] = -np.sqrt(2) * d * (Ct/Ixx)
-# B[11, 1] = -np.sqrt(2) * d * (Ct/Ixx)
-# B[11, 2] = np.sqrt(2) * d * (Ct/Ixx)
-# B[11, 3] = np.sqrt(2) * d * (Ct/Ixx)
+B = np.zeros((N, M))
+B[8, 0] = 2. * (Ct/m)
+B[8, 1] = 2. * (Ct/m)
+B[8, 2] = 2. * (Ct/m)
+B[8, 3] = 2. * (Ct/m)
+B[9, 0] = -2. * (Cd/Izz)
+B[9, 1] = 2. * (Cd/Izz)
+B[9, 2] = -2. * (Cd/Izz)
+B[9, 3] = 2. * (Cd/Izz)
+B[10, 0] = -np.sqrt(2) * d * (Ct/Iyy)
+B[10, 1] = np.sqrt(2) * d * (Ct/Iyy)
+B[10, 2] = np.sqrt(2) * d * (Ct/Iyy)
+B[10, 3] = -np.sqrt(2) * d * (Ct/Iyy)
+B[11, 0] = -np.sqrt(2) * d * (Ct/Ixx)
+B[11, 1] = -np.sqrt(2) * d * (Ct/Ixx)
+B[11, 2] = np.sqrt(2) * d * (Ct/Ixx)
+B[11, 3] = np.sqrt(2) * d * (Ct/Ixx)
 
 # C = np.eye(12)
