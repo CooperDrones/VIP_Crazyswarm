8f71f6d9c6d4b37a440105671e5857744e8f827d
Justin Rooney <rooneyjc@gmail.com>
2020-02-12 20:34:26 -0500

    feat: vicon cf pose obtained through topic
    requires additional debugging

--- crazyflie_demo/scripts/a_hover_stiff_cf3.py
+++ crazyflie_demo/scripts/a_hover_stiff_cf3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-from a_hover_stiff import CooperativeQuad
+from a_hover_stiff_multi import CooperativeQuad
 import rospy
 
 if __name__ == '__main__':
-    # rospy.init_node('cooperative_quad3', anonymous=True)
     cf3 = CooperativeQuad('crazyflie3')
     cf3.hoverStiff(0.0, 0.0, 0.4, 0.0, 0.1)
-    cf3.land()
+    # cf3.land()
