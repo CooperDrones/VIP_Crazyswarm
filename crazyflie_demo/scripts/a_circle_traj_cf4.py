2c311ceebb70a9a95306bc243496a66ce2a62b3f
Justin Rooney <rooneyjc@gmail.com>
2020-03-06 15:22:51 -0500

    feat: circle trajectories start postion
    can define center of cirle traj and drone start along traj. Trying to get CW versus CWW feature working next

--- crazyflie_demo/scripts/a_circle_traj_cf4.py
+++ crazyflie_demo/scripts/a_circle_traj_cf4.py
@@ -10,19 +10,26 @@ from pytz import timezone
 if __name__ == '__main__':
     # Initial Hover start point
     z_c = 0.4
-    y_c = 0.0
+    y_c = 0.5
     x_c = 0.5
     yaw_c = 0.0
     cf4 = CooperativeQuad('crazyflie4')
     
-    traj = TrajGenerator()
-    radius = 0.5
-    omega = 2.0
+    # traj_gen = TrajGenerator()
+    # radius = 0.5
+    # omega = 2.0
+    # no_osc = 2.0
+    # phase_shift = 0.0
+    # circle_traj = traj_gen.genCircleTraj(x_c, y_c, z_c, radius, \
+    #     omega, no_osc, phase_shift)
+
+    traj_gen = TrajGenerator()
+    x_center = 0.0; y_center = 0.0
+    omega = 0.5
     no_osc = 2.0
-    phase_shift = 0.0
-    circle_traj = traj.genCircleTraj(x_c, y_c, z_c, radius, \
-        omega, no_osc, phase_shift)
-    
+    circle_traj = traj_gen.genCircleTraj(x_c, y_c, x_center, y_center, \
+        omega, no_osc, CCW=False)
+
     cf4.hoverStiff(x_c, y_c, z_c, yaw_c, 0.05)
     cf4.trajTracking(circle_traj, z_c)
     cf4.hoverStiff(x_c, y_c, z_c, yaw_c, 0.1)
--- crazyflie_demo/scripts/a_traj_generator.py
+++ crazyflie_demo/scripts/a_traj_generator.py
@@ -81,7 +81,8 @@ class TrajGenerator:
         self.hz = hz
         self.t_phys = 1/self.hz
     
-    def genCircleTraj(self, x_c, y_c, z_c, r, omega, no_osc, phase_shift, show_plots=False):
+    def genCircleTraj(self, x_c, y_c, x_center, y_center, \
+        omega, no_osc, CCW=True, show_plots=False):
         """
         Generate a circular trajectory with form
         [ x, xd, xdd, y, yd, ydd, z, zd, zdd ]
@@ -96,39 +97,49 @@ class TrajGenerator:
 
         T = (2.0 * np.pi)/omega # Period
         t_end = no_osc * T # simulation run time
-        print(t_end)
+        # print(t_end)
 
         t = 0.0
         time_list = []
 
         no_steps = int(t_end/self.t_phys)
-        print(no_steps)
+        # print(no_steps)
 
         traj = np.zeros((no_steps, 9))
 
+        r = np.sqrt((x_c - x_center)**2 + (y_c - y_center)**2)
+        x_temp = x_c - x_center; y_temp = y_c - y_center 
+        ang = -np.arctan2(y_temp, x_temp)
+        # print('angle is {}'.format(ang))
+
+        # Flies Drone CCW when viewing from above
+        if CCW == False:
+            # sign = -1.0
+            ang += np.pi
+
         for idx in range(no_steps):
-            x   = r * np.cos(omega * t)
-            xd  = r * omega * -np.sin(omega * t) 
-            xdd = r * omega**2 * -np.cos(omega * t)
-            y   = r * np.sin(omega * t)
-            yd  = r * omega * np.cos(omega * t) 
-            ydd = r * omega**2 * -np.sin(omega * t)
+            x   = r * np.cos(omega * t - ang) + x_center
+            xd  = r * omega * -np.sin(omega * t - ang) 
+            xdd = r * omega**2 * -np.cos(omega * t - ang)
+            y   = r * np.sin(omega * t - ang) + y_center
+            yd  = r * omega * np.cos(omega * t - ang) 
+            ydd = r * omega**2 * -np.sin(omega * t - ang)
             traj[idx] = np.array([x, xd, xdd, y, yd, ydd, 0.0, 0.0, 0.0])
             time_list.append(t)
             t += self.t_phys
-
-        # print(traj)
 
         if show_plots:
             num_rows = 2; num_cols = 1
             fig, ax = plt.subplots(num_rows, num_cols, figsize=(6,12))
 
-            ax[0].plot(traj[:,0], traj[:,3], c='r', label='pos')
-            ax[0].scatter(x_c, y_c, marker='x', s=200)
+            ax[0].plot(traj[:,0], traj[:,3], c='r')
+            ax[0].scatter(x_center, y_center, marker='x', c='r', s=100)
+            ax[0].scatter(x_c, y_c, marker='x', c='b', s=100, label='drone start')
             # ax[0].plot(traj[:,3], traj[:,1], c='b', label='vel')
             ax[0].set_title('Circle Trajectory')
             ax[0].set_ylabel('y-pos [m]')
             ax[0].set_xlabel('x-pos [m]')
+            ax[0].legend()
 
             ax[1].plot(time_list, traj[:,0], c='r', label='pos')
             ax[1].plot(time_list, traj[:,1], c='b', label='vel')
@@ -149,14 +160,13 @@ def plotWaveTraj():
     plt.show()
 
 def plotCircleTraj():
-    circle_traj = TrajGenerator()
-    x_c = 0.5; y_c = 0.0; z_c = 0.4
-    radius = 0.5
+    traj_gen = TrajGenerator()
+    x_c = 0.5; y_c = 0.0
+    x_center = 0.0; y_center = 0.0
     omega = 1.0
     no_osc = 2.0
-    phase_shift = 0.0
-    traj = circle_traj.genCircleTraj(x_c, y_c, z_c, radius, \
-        omega, no_osc, phase_shift, True)
+    circle_traj = traj_gen.genCircleTraj(x_c, y_c, x_center, y_center, \
+        omega, no_osc, CCW=False, show_plots=True)
     plt.show()
 
 if __name__ == "__main__":
